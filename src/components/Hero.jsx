6970994399f6172c9d58684b3679e4fa00c9ba26
Akash290104 <mishra.ak801@gmail.com>
2025-03-13 12:47:22 +0530

    screen adjust

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className="mt-10 border-b border-neutral-900 pb-4 lg:mb-16 sm:mt-20">
+    <div className="pt-24 sm:pt-28 border-b border-neutral-900 pb-4 lg:mb-16 ">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ scrollToProjects, scrollToContact }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className=" fixed top-0 left-0 w-full bg-gradient-to-r from-blue-600 to-purple-500 text-white shadow-md z-50">
+    <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-blue-600 to-purple-500 text-white shadow-md z-50">
       <div className="flex items-center justify-between px-6 py-4">
         {/* Menu Button (Left-Aligned) */}
         <button onClick={() => setIsOpen(true)} className="text-white text-3xl">
@@ -43,6 +43,12 @@ const Navbar = ({ scrollToProjects, scrollToContact }) => {
             className="text-lg font-medium border-b-2 border-transparent hover:border-white transition"
           >
             Contact
+          </button>
+          <button
+            onClick={scrollToProjects}
+            className="flex items-center text-lg gap-2 cursor-pointer border-b-2 border-transparent hover:border-white transition"
+          >
+            <FaProjectDiagram /> See My Projects
           </button>
           <a
             href="https://bit.ly/4hlSD0l"
